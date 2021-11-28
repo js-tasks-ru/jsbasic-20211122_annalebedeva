213b4af2bdd12f544399df0ca13770d7492baa28
Anna Lebedeva <anna.chalyshova@mail.ru>
2021-11-28 13:10:03 +0300

    Расчет факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,9 @@
 function factorial(n) {
-  // ваш код...
+  if ( n == 0 || n == 1 ) {
+    return 1;
+  }
+  for (let i = n - 1; i >= 1; i--) { // i = n - 1, n -2 , n - 3, ..., 1
+    n *= i;                          
+   }
+   return n;
 }
