99171cc9024a7d80f25129e2fccb15aafbf60703
Anna Lebedeva <anna.chalyshova@mail.ru>
2021-12-08 13:34:41 +0300

    Перевод текста вида border-left-width в borderLeftWidth

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  let words = str.split('-');
+  let newWords = words.map((item, index) => {
+    if (index == 0) {
+      return item;
+    } else {
+      item = item[0].toUpperCase() + item.slice(1);
+      return item;
+    }
+  });
+  let str2 = newWords.join('');
+  return str2;
 }
