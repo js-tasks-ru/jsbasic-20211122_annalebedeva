93b6f27a1f570bb2665714bb5bbd2b91f740d68e
Anna Lebedeva <anna.chalyshova@mail.ru>
2021-12-12 17:04:16 +0300

    Функция makeFriendsList(friends)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let fullNames = '';
+
+  for (let i = 0; i < friends.length; i++) {
+    fullNames += `<li>${friends[i].firstName} ${friends[i].lastName}</li>`;
+  }
+
+  let ul = document.createElement('ul');
+  ul.innerHTML = fullNames;
+
+  return ul;
 }
