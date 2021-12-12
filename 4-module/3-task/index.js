7c2ddf9583ea414a220a01fdbcdac1cad9e4fa42
Anna Lebedeva <anna.chalyshova@mail.ru>
2021-12-12 23:43:30 +0300

    Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.tBodies[0].rows;
+  for (let i = 0; i < rows.length; i++) {
+      // Добавляем класс
+      if (rows[i].cells[3].getAttribute('data-available') == "true") {
+        rows[i].classList.add('available')
+      } else if (rows[i].cells[3].getAttribute('data-available') == "false") {
+        rows[i].classList.add('unavailable')
+      } else {
+        rows[i].setAttribute('hidden', true);
+      }
+
+      // gender
+      if (rows[i].cells[2].textContent == "m") {
+        rows[i].classList.add('male')
+      } else {
+        rows[i].classList.add('female')
+      }
+
+      // style
+      if (Number(rows[i].cells[1].textContent) < 18) {
+        rows[i].style="text-decoration: line-through"
+      }
+  }
 }
