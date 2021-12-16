7a9f37cd871479523b1594191639d1d726c572a1
Anna Lebedeva <anna.chalyshova@mail.ru>
2021-12-16 21:57:19 +0300

    Скрыть элемент по нажатию кнопки

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button');
+  const div = document.querySelector('#text');
+  btn.addEventListener('click', () => {
+    if (div.hasAttribute('hidden')) {
+      div.removeAttribute('hidden');
+    } else {
+      div.setAttribute('hidden', true);
+    }
+  });
 }
