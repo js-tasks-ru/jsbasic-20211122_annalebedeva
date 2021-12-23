7622ad5986ef59ae185916198b019130e0152ba2
Anna Lebedeva <anna.chalyshova@mail.ru>
2021-12-23 22:22:46 +0300

    Создание таблицы

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,38 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
-export default class UserTable {
+class UserTable {
   constructor(rows) {
+    const table = document.createElement('table');
+
+    const content = rows.map(rowsItem => {
+      return `<tr>
+        <td>${rowsItem.name}</td>
+        <td>${rowsItem.age}</td>
+        <td>${rowsItem.salary}</td>
+        <td>${rowsItem.city}</td>
+        <td><button>X</button></td>
+        </tr>`;
+    });
+
+    table.innerHTML = `<thead>
+    <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+    </tr>
+    </thead><tbody>${content.join('')}</tbody>`
+
+    table.addEventListener('click', this.onClick);
+
+    this.elem = table;
+  }
+
+  onClick(event) {;
+    if (event.target.tagName === 'BUTTON') {
+      const td = event.target.parentNode;
+      td.parentNode.remove();
+    }
   }
 }
+
+export default UserTable;
